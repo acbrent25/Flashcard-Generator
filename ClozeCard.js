7d0a058d8e301668b89aa9462b44715399d2faf5
Adam Champagne <adamchampagne@Adams-iMac.fios-router.home>
2017-10-12 07:12:49 -0400

    working

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,8 +1,6 @@
 var ClozeCard = function(text, cloze){
     this.text = text;
     this.cloze = cloze;
-    this.partial = partial;
-    this.fullText = fullText;
 }
 
 module.exports = ClozeCard;
--- flashCard.js
+++ flashCard.js
@@ -8,8 +8,11 @@ var basicCardArr = [];
 var basicCardFront;
 var basicCardBack;
 var basicCard;
+var partial;
 
-
+/************************
+  CHOOSE CARD TYPE
+*************************/
 inquirer.prompt([
     
       {
@@ -21,32 +24,30 @@ inquirer.prompt([
     
     ]).then(function(card) {
     
-      // If the user guesses the password...
       if (card.chooseCardType === "Basic") {
     
         console.log("==============================================");
-        console.log("Basic Bitch");
+        console.log("Basic Card");
         console.log("==============================================");
 
         createBasicCard();
         
       }
     
-    
-      // If the user doesn't guess the password...
       else {
     
         console.log("==============================================");
-        console.log("Not so Basic");
+        console.log("Cloze Card");
         console.log("==============================================");
+
+        createClozeCard();
     
       }
     });
     
-
-/*********************
+/************************
     CREATE BASIC CARD
-**********************/
+************************/
 
 function createBasicCard(){
     inquirer.prompt([
@@ -63,42 +64,81 @@ function createBasicCard(){
         },
         ]).then(function(basicText) {
            
-            console.log("card front: " + basicText.cardFront);
-            console.log("card Back: " + basicText.cardBack);
-            basicCardFront = basicText.cardFront;
-            basicCardBack = basicText.cardBack;
-            // basicCard = "Front: " + basicCardFront + " Back: "  + basicCardBack;           
-            addBasicText();
+            var newBasicCard = new BasicCard (basicText.cardFront, basicText.cardBack)
+            fs.appendFile("basiccard.txt", "Flashcard front: " + basicText.cardFront + ", "  + " Flashcard Back: " +  basicText.cardBack + "\r\n", function(err) {
+              if (err) {
+                return console.log(err);
+              }
+            })
+            // Print out current card
+            console.log("==============================================");
+            console.log("Current Card");
+            console.log("------------");
+            console.log("Flashcard front: " + basicText.cardFront + ", "  + " Flashcard Back: " +  basicText.cardBack);
+            console.log("\n\r");
+            console.log("==============================================");
+            
+            readCards();
+            
+          }
+        )
+      };
 
-        });
-}
+/************************
+    CREATE BASIC CARD
+************************/
+    function createClozeCard(){
+      inquirer.prompt([
+          
+          {
+              type: "input",
+              name: "text",
+              message: "Please enter the full text"
+          },
+          {
+              type: "input",
+              name: "cloze",
+              message: "Please enter the cloze"
+          },
+          ]).then(function(clozeText) {
+              console.log(clozeText.text)
+              var newClozeCard = new ClozeCard (clozeText.text, clozeText.cloze)
+              
+              partial = clozeText.text.replace(clozeText.cloze, "_____________");
+              
+              
+              fs.appendFile("basiccard.txt", "Full Text: " + clozeText.text + ", "  + " Cloze: " +  clozeText.cloze + "\r\n", function(err) {
+                if (err) {
+                  return console.log(err);
+                }
+              })
+              // Print out current card
+              console.log("==============================================");
+              console.log("Current Card");
+              console.log("------------");
+              console.log("Flashcard front: " + partial + ", "  + "\n\r" + " Flashcard Back: " +  clozeText.text);
+              console.log("\n\r");
+              console.log("==============================================");
+              
+              readCards();
+              
+            }
+          )
+        };
 
-function addBasicText() {
-
-      // We will add the value to the bank file.
-      fs.appendFile("basiccard.txt", basicCardFront, function(err) {
-        if (err) {
-          return console.log(err);
-        }
-      });
-
-    
-      // We will then print the value that was added (but we wont print the total).
-      // console.log("FlashCard " + basicCardArr + ".");
-      readBasicCard();
-    }
-
-    function readBasicCard() {
+    function readCards() {
       
-        // We will read the existing bank file
+        // Read the existing bank file
         fs.readFile("basiccard.txt", "utf8", function(err, cardData) {
           if (err) {
             return console.log(err);
           }
-      
-          // Break down all the numbers inside
-          // flashcard = cardData.split("|");
+          // Print out all cards
+          console.log("==============================================");
+          console.log("All Cards");
+          console.log("----------");
           console.log(cardData);
+          console.log("==============================================");
 
         });
       }
